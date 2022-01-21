a4c34dd2e3bf2650e369b9cd613aabccd15da74e
Shakhram <shakag6@gmail.com>
2022-01-21 14:33:16 -0500

    Made home.js. Closes #1

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Home from './Components/Home';
 
-function App() {
+export default function App() {
+  const [route, setRoute] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-  );
-}
+  )
 
-export default App;
+}
---
+++ bank-of-react/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default function Home () {
+
+    return (
+        <div>
+            <img src="https://i1.wp.com/fintechranking.com/wp-content/uploads/2016/03/fintech-01.png?fit=2613%2C1503&ssl=1" alt="bank" />
+            <h1>Bank of React</h1>
+        </div>
+    )
+    
+}
